ea34244675931c236ac816b4bcc0277c209d4c54
will7925 <will.twomey@RACKSPACE.COM>
2018-10-06 21:50:18 -0500

    v0.1

--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 
-define('LDAP_SERVER', 'bbis.us');
+define('LDAP_SERVER', 'example.com');
 define('LDAP_PORT', 389);
 
-// dn: <key>=username
-// Some servers use 'cn'
+// dn: <key>=comername
+// Some servers come 'cn'
 define('LDAP_KEY', 'uid');
 
-define('LDAP_SEARCH', 'ou=People,dc=bbis,dc=us');
+define('LDAP_SEARCH', 'ou=People,dc=example,dc=com');
 
 // Set LDAP version, v3 is required for recent versions of OpenLDAP
 define('LDAP_VERSION', 3);
 
-//Set the user ID to impersonate, by default admin is 1
+//Set the comer ID to impersonate, by default admin is 1
 define('LDAP_IMPERSONATE_ID', 1);
 
-//You can limit logins to a certain group, comment to allow all LDAP users
+//You can limit logins to a certain group, comment to allow all LDAP comers
 define('LDAP_GROUP', 'cn=wpadmin');
-define('LDAP_GROUP_BASE', 'ou=Group,dc=bbis,dc=us');
+define('LDAP_GROUP_BASE', 'ou=Group,dc=example,dc=com');
 define('LDAP_GROUP_VALUE', 'memberuid');
 ?>
